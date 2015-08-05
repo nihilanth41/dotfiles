87786f3b072cbd28a2643657168a0e36f37ca85c
Zachary Rump <nihilanth41@gmail.com>
2015-08-05 16:04:48 -0500

    Working on bash script

--- expand_dotfiles.sh
+++ expand_dotfiles.sh
@@ -3,7 +3,14 @@
 FILES=$HOME/dotfiles/*
 for file in $FILES 
 do
+	if [ "$file" == "expand_files.sh" ]; then
+		continue; 
+	fi
 	echo "Processing $file file..."
+	#get name after last '/' in path and prepend a '.' 
+	DOTFILE=.${file##*/} 
+	echo "Dotfile is: $DOTFILE"
+	
 	#if file doesn't exist
 	#{
 	#	symlink $file to ~/.$file
